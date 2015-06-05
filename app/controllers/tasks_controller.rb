439085ea0d890cb1b845a6c7789087cdc9ea43ae
Tammam Kbeili <tammam.kbeili@gmail.com>
2015-06-05 15:54:31 -0700

    implement new / create / show / index

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task_params = params.require(:task).permit(:title)
+    @task = Task.new task_params
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @task = Task.find params[:id]
+  end
+
+end
